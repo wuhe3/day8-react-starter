11b353919852483ff726d6d94b47ad2df1aac9e9
wuhe3 <wuhe3@oocl.com>
2024-11-27 20:05:03 +0800

    feat, add updateCounterValue

--- src/components/CounterGroup.jsx
+++ src/components/CounterGroup.jsx
@@ -4,10 +4,21 @@ import {useState} from "react";
 const CounterGroup = ({size, setSum}) => {
     const [counterValues, setCounterValues] = useState({});
 
+    const updateCounterValue = (id, value) => {
+        setCounterValues(prev => ({
+            ...prev,
+            [id]: value
+        }));
+    };
+
     return (
         <div className={"counter-group-wrapper"}>
             {Array.apply(null, { length: size }).map((e, i) => (
-                <Counter id={`counter${i}`} key={`counter${i}`} />
+                <Counter
+                    id={`counter${i}`}
+                    key={`counter${i}`}
+                    onValueChange={updateCounterValue}
+                />
             ))}
         </div>
     )
