513de77b5b63210ce3854ca6f4a8a9ac7075afd6
wuhe3 <wuhe3@oocl.com>
2024-11-27 21:15:06 +0800

    refactor: update CounterGroupGenerator, use onReset prop

--- src/components/CouterGroupGenerator.jsx
+++ src/components/CouterGroupGenerator.jsx
@@ -1,34 +1,33 @@
 import {useState} from "react";
 
-const CounterGroupGenerator = (props) => {
-    const [size, setSize] = useState(props.size);
-    const sum = size;
+const CounterGroupGenerator = ({size, onReset}) => {
+    const [inputSize, setInputSize] = useState(size);
 
     const handleChange = (event) => {
         let newSize = event.target.value;
 
         if (newSize > 20) {
-            newSize = 20
+            newSize = 20;
         }
 
         if (newSize < 0) {
-            newSize = 0
+            newSize = 0;
         }
 
-        setSize(newSize)
+        setInputSize(newSize);
     };
 
     const handleReset = () => {
-        props.setSize(size);
-    }
+        onReset(inputSize);
+    };
 
     return (
         <div>
-            <span>Size: {props.size}</span>
-            <input min={0} max={20} type="number" value={size} onChange={handleChange}/>
+            <span>Size: {size}</span>
+            <input min={0} max={20} type="number" value={inputSize} onChange={handleChange}/>
             <button onClick={handleReset}>reset</button>
         </div>
-    )
+    );
+};
 
-}
 export default CounterGroupGenerator;
