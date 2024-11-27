92c2bc2f441a141e6432f2eb33090f842d91df07
WUHE3 <wuhe3@oocl.com>
2024-11-27 15:45:53 +0800

    feat, ADD title

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -1,28 +1,32 @@
 import ProductItem from "./ProductItem";
 
 const ProductCategory = () => {
-    const products = [
-        {
-            name: "Football",
-            price: 49.9,
-            status: true
-        },
-        {
-            name: "Tennis Ball",
-            price: 9.9,
-            status: false
-        },
-        {
-            name: "Basketball",
-            price: 29.9,
-            status: true
-        }
-    ]
+    const productLists = {
+        title: "Sporting Goods",
+        products: [
+            {
+                name: "Football",
+                price: 49.9,
+                status: true
+            },
+            {
+                name: "Tennis Ball",
+                price: 9.9,
+                status: false
+            },
+            {
+                name: "Basketball",
+                price: 29.9,
+                status: true
+            }
+        ]
+    }
 
     return (
         <div>
-            {products.map((product, index) => {
-                return <ProductItem productItem={product} key={product.name + index} />
+            {productLists.title}
+            {productLists.products.map((product, index) => {
+                return <ProductItem productItem={product} key={product.name + index}/>
             })}
         </div>
     )
